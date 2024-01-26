c065a3e43637df0ab2f0ed6bdc2d27bb00e256ff
mehmet.hilmi007@gmail.com <mehmet.hilmi007@gmail.com>
2024-01-26 16:29:55 +0300

    last-commit

---
+++ HealthCenter/urls.py
@@ -0,0 +1,42 @@
+"""
+URL configuration for FizyoSelinus project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+# from Payment.views import *
+# from Payment.urls import *
+
+urlpatterns = [
+    #! Home:
+    path('home/', include('Home.urls')),
+
+    #! AppointmentHub:
+    path('appointment/', include('Appointment.urls')),
+
+    #! Payment:
+    path('payment/', include('Payment.urls') ),
+
+    #! News:
+    path('news/', include('News.urls')),
+
+    #!Team:
+    path('team/', include('Team.urls')),
+
+    path('admin/', admin.site.urls), #!superuser: admin|admin@admin.com|pw:1234
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
